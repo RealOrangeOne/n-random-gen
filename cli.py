bb21dc854403164f371e59d85c353a8426ae9709
Jake Howard <git@theorangeone.net>
2019-08-11 23:48:58 +0100

    Write everything

--- cli.py
+++ cli.py
@@ -1,17 +1,22 @@
 import argparse
+import secrets
 
-def main(count, max_value):
+def main(count, min_value, max_value):
     assert count > 0
-    assert max_value > 0
+    assert min_value < max_value
 
-    print(count, max_value)
-
+    for i in range(count):
+        print("{}: {}".format(
+            i, secrets.choice(range(min_value, max_value + 1))
+        ))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('count', type=int)
+    parser.add_argument('min', type=int)
     parser.add_argument('max', type=int)
 
     args = parser.parse_args()
-    main(args.count, args.max)
+
+    main(args.count, args.min, args.max)
